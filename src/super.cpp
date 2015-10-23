d8f15d806af1f3f4be7ae46260176a0603e8fbf9
Iason Sarantopoulos <iason@sarantop.com>
2015-10-23 18:02:03 +0300

    Write Simple grasp

--- src/super.cpp
+++ src/super.cpp
@@ -35,7 +35,7 @@ void Initialize()
   }
   bh.setHardwareDesc(hwIndex);
   bool use280Config = (strcmp(bh.getHardwareDesc()->getModelNumber(), "BH8-280") == 0);
-  
+
   if (result = handInitWithMenu(&bh))
     Error();
   printf("Initialization...");
@@ -45,10 +45,76 @@ void Initialize()
     printf(" Done\n");
 }
 
-int Execute()
-{
-  printf("Execute: Press Any Key to Abort...");
 
+void doneMoving(char motor)
+{
+  int mode, hold;
+  bh.Get(motor, "MODE", &mode);
+  bh.Get(motor, "HOLD", &hold);
+
+  if (mode!=0 & (mode==3 | hold==0)) 
+    return false;
+
+  return true;
+}
+
+int SimpleGrasp()
+{
+
+cout << "--- [BHand] Simple Grasp started..." << endl;
+
+  int rc;
+
+  int sg[4], sg_cur[4], pos_cur[4];
+  bh.Get("G", "SG", sg);
+ 
+  bh.Set("G", "V", 20);
+  bh.Set("S", "V", 0);
+  bh.Set("G", "TSTOP", "0");
+  bh.Set("G", "MODE", 4);
+  
+/** Start grasping. When every fingers feels the object stop and hold the grasp */
+  bool flag[3] = {true, true, true};
+  flag[2] = false;
+
+  while(flag[0] | flag[1] | flag[2]) 
+  {
+    bh.Get("G", "P", pos_cur);
+    bh.Get("G", "SG", sg_cur);
+    
+    for (int finger=1; finger<=3; finger++) {
+      if (sg_cur[finger-1] - sg[finger-1] > 400 && pos_cur[finger-1] > 80000) 
+      {
+        bh.Set((char)finger, "MODE", 0);
+        flag[finger-1] = false;
+      }
+      
+    }
+
+    int counter = 0;
+    if (doneMoving("1") && doneMoving("2") && doneMoving("3")) counter++;
+
+    if (counter > 1000) {
+      ROS_INFO("[BHand Server] Simple grasp: I didn't feel forces, but I terminated due to abscence of movement");
+      break;
+    }
+  }
+
+  bh.Set("S", "MODE", 0);
+}
+
+int ExecuteHandshake()
+{
+}
+
+
+int ExecuteHandover()
+{
+  printf("Execute Experiment: Press Any Key to Abort...");
+
+  SimpleGrasp();
+
+  bh.Set("1", "M", 100000);
 
   if (result = bh.Close( "S" ))
     Error();
@@ -100,16 +166,32 @@ int main()
   /* Turn on unbuffered input */
   UnbufferedInputStart();
 
-  printf("-- [BHand] Initializing Software...\n");
   Initialize();
-  printf("-- [BHand] Executing - ");
-  //Execute();
-  bh.Set("1", "M", 100000);
-  printf("-- [BHand] Done\n");
+
+  cout << "-- [BHand] Initializing Software..." << endl;
+  cout << "-- [BHand] Which experiment you want to run?" << endl;
+  cout << "-- [BHand] 1: Handshake" << endl;
+  cout << "-- [BHand] 2: Handover" << endl;
+  int option;
+  cin >> option;
+
+  switch (option){
+    case 1:
+      cout << "-- [BHand] Executing handshake experiment" << endl;
+      ExecuteHandshake();
+      break;
+    case 2:
+      cout << "-- [BHand] Executing handover experiment" << endl;
+      ExecuteHandover();
+      break;
+    default:
+      cout << "-- [BHand] Please type a valid experiment id" << endl;
+      return 0;
+  }
+ 
+  cout << "-- [BHand] Done..." << endl;
 
   /* Turn off unbuffered input */
   UnbufferedInputEnd();
   return 0;
-
-  cout << "BHand object created." << endl;
 }
